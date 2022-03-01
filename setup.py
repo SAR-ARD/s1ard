7eb691fe274959a77ddc3c39cb383f2fb98e105f
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2022-03-01 16:15:33 +0100

    use setuptools_scm for detailed versioning

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='S1_NRB',
-    version='0.1.0',
+    setup_requires=['setuptools_scm'],
+    use_scm_version=True,
     description="Prototype processor for the Sentinel-1 Normalized Radar Backscatter (S1 NRB) product",
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -22,7 +23,7 @@ setup(
                       'pyroSAR @ git+https://github.com/johntruckenbrodt/pyroSAR.git',
                       'spatialist>=0.10.0',
                       'scipy'],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     zip_safe=False,
     entry_points={
         'console_scripts': ['s1_nrb=S1_NRB.cli:cli']
