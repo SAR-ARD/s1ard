a43295208734ea3332fe2d172ec3e8b7983c30b0
maawoo <marco.wolsza@uni-jena.de>
2022-04-26 15:27:47 +0200

    [tile_extraction] new function `aoi_from_tiles`

--- S1_NRB/tile_extraction.py
+++ S1_NRB/tile_extraction.py
@@ -1,6 +1,7 @@
 import re
 from lxml import html
 from spatialist.vector import Vector, wkt2vector, bbox
+from S1_NRB.ancillary import get_max_ext
 
 
 def tiles_from_aoi(vectorobject, kml, epsg=None, strict=True):
@@ -51,6 +52,35 @@ def tiles_from_aoi(vectorobject, kml, epsg=None, strict=True):
         geom = None
         item = None
         return sorted(tilenames)
+
+
+def aoi_from_tiles(kml, tiles):
+    """
+    Returns the bounding box of a list of MGRS tile IDs as a :class:`~spatialist.vector.Vector` object.
+    
+    Parameters
+    ----------
+    kml: str
+        Path to the Sentinel-2 tiling grid KML file.
+    tiles: list[str]
+        A list of unique MGRS tile IDs.
+    
+    Returns
+    -------
+    spatialist.vector.Vector
+    
+    Notes
+    -----
+    The global Sentinel-2 tiling grid can be retrieved from:
+    https://sentinel.esa.int/documents/247904/1955685/S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.kml
+    """
+    geometries = []
+    for tile in tiles:
+        geom = extract_tile(kml=kml, tile=tile)
+        geom.reproject(4326)
+        geometries.append(geom)
+    max_ext = get_max_ext(geometries=geometries)
+    return bbox(max_ext, crs=4326)
 
 
 def extract_tile(kml, tile):
