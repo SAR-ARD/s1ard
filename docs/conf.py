f15a99ce66efabe0b67ed3b6c27d0b2dce176586
maawoo <marco.wolsza@uni-jena.de>
2022-04-26 17:42:24 +0200

    [docs] update config

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ extensions = [
 bibtex_bibfiles = ['references.bib']
 
 # autodoc_default_flags = ['members']
-autosummary_generate = []
+autosummary_generate = True
 
 # explicitly link to documentation of the spatialist version installed alongside pyroSAR,
 # which is defined in setup.py and requirements.txt
@@ -125,7 +125,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
