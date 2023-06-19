3b5cef37baff1ea2adb93b1faf5a14a82a708e4b
maawoo <marco.wolsza@uni-jena.de>
2023-06-19 10:41:38 +0200

    [container] replace update_snap.sh with esa-snap.sh
    New script includes installation and update of SNAP

---
+++ docker/esa-snap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget -q -O /src/snap_install.sh "http://step.esa.int/downloads/9.0/installers/esa-snap_all_unix_9_0_0.sh"
+sh /src/snap_install.sh -q -varfile /src/docker/esa-snap.varfile
+
+# https://senbox.atlassian.net/wiki/spaces/SNAP/pages/30539785/Update+SNAP+from+the+command+line
+/usr/local/snap/bin/snap --nosplash --nogui --modules --update-all 2>&1 | while read -r line; do
+    echo "$line"
+    [ "$line" = "updates=0" ] && sleep 2 && pkill -TERM -f "snap/jre/bin/java"
+done
+
+rm -rf /src/snap_install.sh
