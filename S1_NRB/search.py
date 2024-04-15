5495fa2a63080805d14e0d8b01256d8a4ff95c78
John Truckenbrodt <john.truckenbrodt@posteo.de>
2024-04-15 12:08:56 +0200

    [scene_search] do not raise an error when selection is empty

--- S1_NRB/search.py
+++ S1_NRB/search.py
@@ -531,6 +531,8 @@ def scene_select(archive, kml_file, aoi_tiles=None, aoi_geometry=None, **kwargs)
     # derive geometries and tiles from scene footprints
     if vec is None:
         selection_tmp = archive.select(**args)
+        if len(selection_tmp) == 0:
+            return [], []
         if not isinstance(selection_tmp[0], ID):
             scenes = identify_many(scenes=selection_tmp, sortkey='start')
         else:
