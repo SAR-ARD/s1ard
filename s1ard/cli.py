e18f95193982ef41337cac536c387a6f5253cd5a
John Truckenbrodt <john.truckenbrodt@posteo.de>
2024-09-04 14:39:46 +0200

    start processing with 's1rb process'; new option `s1rb init` to initialize config file

--- s1ard/cli.py
+++ s1ard/cli.py
@@ -1,21 +1,96 @@
 import click
+import importlib.resources
 
 
-@click.command(name='s1rb',
-               no_args_is_help=True,
-               context_settings=dict(
-                   ignore_unknown_options=True,
-                   allow_extra_args=True, )
-               )
+@click.group(name='s1rb',
+             no_args_is_help=True,
+             invoke_without_command=True)
+@click.option('--version', is_flag=True,
+              help='Print s1ard version information and exit. Overrides all other arguments.')
+def cli(version):
+    if version:
+        import s1ard
+        print(s1ard.__version__)
+
+
+@cli.command(name='init',
+             no_args_is_help=True,
+             context_settings=dict(
+                 ignore_unknown_options=True,
+                 allow_extra_args=True, )
+             )
+@click.option('--config-file', '-c', required=True, type=click.Path(),
+              help="Full path to an INI-style target configuration text file.")
+@click.option('--config-source', '-s', required=False, type=click.Path(),
+              help="Full path to an INI-style source configuration text file. "
+                   "If not defined, configuration will be read from the package's default file.")
+@click.pass_context
+def init(ctx, config_file, config_source=None):
+    """
+    Initialize configuration for s1ard processing.
+
+    This creates a processor configuration file in a user-defined location.
+    The package#s default file or a user-defined source file may serve as base.
+    Additional options can be passed to override individual processing parameters
+    in the config file. For example, to read all values from the default
+    file except the acquisition mode and the annotation layers:
+
+    s1rb init -c config.ini --acq_mode IW --annotation dm,id
+    
+    A previously created config file can be used instead of the default file:
+    
+    s1rb init -c config.ini -s config_source.ini --acq_mode IW --annotation dm,id
+
+    The snap_gpt_args argument can be provided by using a dedicated argument
+    separator and quotes:
+
+    s1rb init -c config.ini -- --snap_gpt_args "-J-Xmx100G -c 75G -q 30"
+
+    \b
+    The following defaults are set:
+    PROCESSING
+    - annotation:        dm,ei,id,lc,li,np,ratio
+    - dem_type:          Copernicus 30m Global DEM
+    - date_strict:       True
+    - etad:              False
+    - etad_dir:          None
+    - gdal_threads:      4
+    - snap_gpt_args:     None
+    - measurement:       gamma
+    - ard_dir:           ARD
+    - sar_dir:           SAR
+    - tmp_dir:           TMP
+    - wbm_dir:           WBM
+    - logfile:           None
+    METADATA
+    - access_url:        None
+    - doi:               None
+    - licence:           None
+    - processing_center: None
+    """
+    from s1ard.config import get_config, write
+    extra = {ctx.args[i][2:]: ctx.args[i + 1] for i in range(0, len(ctx.args), 2)}
+    if config_source is None:
+        with importlib.resources.path(package='s1ard.resources',
+                                      resource='config.ini') as path:
+            config_source = str(path)
+    config = get_config(config_file=config_source, **extra)
+    write(config, config_file)
+
+
+@cli.command(name='process',
+             no_args_is_help=True,
+             context_settings=dict(
+                 ignore_unknown_options=True,
+                 allow_extra_args=True, )
+             )
 @click.option('--config-file', '-c', required=False, type=click.Path(),
               help="Full path to an INI-style configuration text file. "
                    "If not defined, the package's default file will be used.")
 @click.option('--debug', is_flag=True,
               help='Print debugging information for pyroSAR modules.')
-@click.option('--version', is_flag=True,
-              help='Print s1ard version information and exit. Overrides all other arguments.')
 @click.pass_context
-def cli(ctx, config_file, debug, version):
+def process(ctx, config_file, debug):
     """
     Central s1ard radar backscatter (rb) processing command.
     
@@ -25,11 +100,11 @@ def cli(ctx, config_file, debug, version):
     in the configuration file. For example, to read all values from the configuration
     file except the acquisition mode and the annotation layers:
     
-    s1rb -c config.ini --acq_mode IW --annotation dm,id
+    s1rb process -c config.ini --acq_mode IW --annotation dm,id
     
     The snap_gpt_args argument can be provided by using a dedicated argument separator and quotes:
     
-    s1rb -c config.ini -- --snap_gpt_args "-J-Xmx100G -c 75G -q 30"
+    s1rb process -c config.ini -- --snap_gpt_args "-J-Xmx100G -c 75G -q 30"
     
     \b
     The following defaults are set:
@@ -53,9 +128,6 @@ def cli(ctx, config_file, debug, version):
     - licence:           None
     - processing_center: None
     """
-    import s1ard
-    if version:
-        print(s1ard.__version__)
-    else:
-        extra = {ctx.args[i][2:]: ctx.args[i + 1] for i in range(0, len(ctx.args), 2)}
-        s1ard.process(config_file=config_file, debug=debug, **extra)
+    from s1ard import process
+    extra = {ctx.args[i][2:]: ctx.args[i + 1] for i in range(0, len(ctx.args), 2)}
+    process(config_file=config_file, debug=debug, **extra)
