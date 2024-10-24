27dce906478606496fb2d3225a012e57f6b11784
John Truckenbrodt <john.truckenbrodt@posteo.de>
2024-10-24 16:30:41 +0200

    [snap.process] hard-lock all preprocessed input products for GRD buffering
    because their metadata may be modified

--- s1ard/snap.py
+++ s1ard/snap.py
@@ -709,7 +709,7 @@ def process(scene, outdir, measurement, spacing, dem,
         out_buffer_wf = out_buffer.replace('.dim', '.xml')
         if not os.path.isfile(out_buffer):
             log.info('buffering GRD scene with neighboring acquisitions')
-            with LockCollection(out_pre_neighbors, soft=True):
+            with LockCollection(out_pre_neighbors + [out_pre] + [out_buffer]):
                 try:
                     grd_buffer(src=out_pre, dst=out_buffer, workflow=out_buffer_wf,
                                neighbors=out_pre_neighbors, gpt_args=gpt_args,
