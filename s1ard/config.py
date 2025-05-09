48a21710f6acbbed954a3d38fb2b629f35a93614
John Truckenbrodt <john.truckenbrodt@posteo.de>
2025-05-09 17:13:29 +0200

    enable processing of Sentinel-1C and -1D data

--- s1ard/config.py
+++ s1ard/config.py
@@ -148,7 +148,7 @@ def get_config(config_file=None, **kwargs):
             if date_short:
                 v += timedelta(days=1, microseconds=-1)
         if k == 'sensor':
-            assert v in ['S1A', 'S1B']
+            assert v in ['S1A', 'S1B', 'S1C', 'S1D']
         if k == 'acq_mode':
             assert v in ['IW', 'EW', 'SM']
         if k == 'work_dir':
