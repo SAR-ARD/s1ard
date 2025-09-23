20afaf1a3a07776fa64ff74d444c2c120184889d
John Truckenbrodt <john.truckenbrodt@posteo.de>
2025-09-23 12:49:02 +0200

    [metadata.mapping] corrected MGRS KML URL

--- s1ard/metadata/mapping.py
+++ s1ard/metadata/mapping.py
@@ -299,8 +299,8 @@ def get_release_version(version_str: str) -> str:
 
 
 URL = {
-    'ancillaryData_KML': 'https://sentinel.esa.int/documents/247904/1955685/S2A_OPER_GIP_TILPAR_MPC__'
-                         '20151209T095117_V20150622T000000_21000101T000000_B00.kml',
+    'ancillaryData_KML': 'https://sentiwiki.copernicus.eu/__attachments/1692737/'
+                         'S2A_OPER_GIP_TILPAR_MPC__20151209T095117_V20150622T000000_21000101T000000_B00.zip',
     'card4l_nrb': 'https://ceos.org/ard/files/PFS/NRB/v5.5/CARD4L-PFS_NRB_v5.5.pdf',
     'card4l_orb': 'https://ceos.org/ard/files/PFS/ORB/v1.0/'
                   'CARD4L_Product_Family_Specification_Ocean_Radar_Backscatter-v1.0.pdf',
